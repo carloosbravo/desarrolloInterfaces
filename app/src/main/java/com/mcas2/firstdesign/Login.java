62d256529d1ddb68234a7d7933a322e0e9fe6180
carloosbravo <carloosbravo04@gmail.com>
2023-11-15 14:11:38 +0100

    Splash screen and AlertView hecho

--- app/src/main/java/com/mcas2/firstdesign/Login.java
+++ app/src/main/java/com/mcas2/firstdesign/Login.java
@@ -1,7 +1,16 @@
 package com.mcas2.firstdesign;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
+
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +18,36 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+
+    public void showAlertviewContraseña(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Recuperación de contraseña")
+                .setMessage("Introduce tu email");
+
+        EditText editTextRecuperarContra = new EditText(this);
+        editTextRecuperarContra.setInputType(InputType.TYPE_CLASS_TEXT);
+        editTextRecuperarContra.setHint("tuemail@example.com");
+        builder.setView(editTextRecuperarContra);
+
+        builder.setPositiveButton("Enviar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Acción cuando se hace clic en el botón Aceptar
+                Toast.makeText(Login.this, "Comprueba tu correo", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Acción cuando se hace clic en el botón Cancelar
+                Toast.makeText(Login.this, "Cancelado", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
--- app/src/main/java/com/mcas2/firstdesign/SplashScreen.java
+++ app/src/main/java/com/mcas2/firstdesign/SplashScreen.java
@@ -1,18 +1,30 @@
 package com.mcas2.firstdesign;
 
+import static com.bumptech.glide.load.resource.bitmap.TransformationUtils.centerCrop;
+import static com.bumptech.glide.request.RequestOptions.bitmapTransform;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestBuilder;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestOptions;
+
+import jp.wasabeef.blurry.Blurry;
+import jp.wasabeef.glide.transformations.BlurTransformation;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -23,23 +35,22 @@ public class SplashScreen extends AppCompatActivity {
         openLogin();
 
         ImageView logoSplash = findViewById(R.id.logoSplash);
-        TextView appNameSplash = findViewById(R.id.appNameSplash);
 
         Animation fadeAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
-        appNameSplash.startAnimation(fadeAnimation);
+        Animation zoomInAnimation = AnimationUtils.loadAnimation(this, R.anim.zoom_in);
 
-        Animation shakeAnimation = AnimationUtils.loadAnimation(this, R.anim.shake);
-        logoSplash.startAnimation(shakeAnimation);
+        logoSplash.startAnimation(zoomInAnimation);
 
         ImageView background = findViewById(R.id.backgroundSplash);
-        Glide.with(this)
-                .load("https://images.unsplash.com/photo-1582103287241-2762adba6c36?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
-                .transition(DrawableTransitionOptions.withCrossFade(2000))
-                .centerCrop()
-                .into(background);
+        RequestBuilder<Drawable> a = Glide.with(this).load("https://images.unsplash.com/photo-1582103287241-2762adba6c36?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+
+        a.centerCrop().into(background);
+        a.transform(new BlurTransformation( 20, 2)).centerCrop().into(background);
+
+        a.transform(new CenterCrop(), new BlurTransformation(20, 2));
     }
 
-    public void openLogin(){
+    public void openLogin() {
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
